d5773b7ba26bc7430a0f6d1976a8832e35274010
Stanislav Golubtsov <sgaflv@gmail.com>
2016-06-30 23:03:07 +0200

    fixed focus issue

--- gef4.mvc.tutorial5/src/gef4/mvc/tutorial/parts/TextNodePart.java
+++ gef4.mvc.tutorial5/src/gef4/mvc/tutorial/parts/TextNodePart.java
@@ -1,6 +1,5 @@
 package gef4.mvc.tutorial.parts;
 
-import java.beans.PropertyChangeEvent;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,15 +9,19 @@ import org.eclipse.gef4.geometry.planar.Rectangle;
 import org.eclipse.gef4.geometry.planar.RoundedRectangle;
 import org.eclipse.gef4.mvc.fx.parts.AbstractFXContentPart;
 import org.eclipse.gef4.mvc.fx.policies.FXTransformPolicy;
+import org.eclipse.gef4.mvc.models.FocusModel;
+import org.eclipse.gef4.mvc.parts.IContentPart;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
+import com.google.common.reflect.TypeToken;
 
 import gef4.mvc.tutorial.model.TextNode;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Bounds;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
@@ -38,39 +41,59 @@ public class TextNodePart extends AbstractFXContentPart<StackPane> {
 
 	private final ChangeListener<Object> objectObserver = new ChangeListener<Object>() {
 		@Override
-		public void changed(ObservableValue<? extends Object> observable,
-				Object oldValue, Object newValue) {
-			
+		public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
+
 			refreshVisual();
 		}
 	};
-	
+
+	private class FocusListener implements ChangeListener<IContentPart<Node, ? extends Node>> {
+
+		private final TextNodePart nodePart;
+
+		FocusListener(TextNodePart nodePart) {
+			this.nodePart = nodePart;
+		}
+
+		@Override
+		public void changed(ObservableValue<? extends IContentPart<Node, ? extends Node>> observable,
+				IContentPart<Node, ? extends Node> oldValue, IContentPart<Node, ? extends Node> newValue) {
+
+			if (nodePart != newValue) {
+				editModeEnd(false);
+			}
+		}
+	}
+
+	private ChangeListener<IContentPart<Node, ? extends Node>> focusObserver = new FocusListener(this);
+
+	@SuppressWarnings("serial")
 	@Override
 	protected void doActivate() {
 		super.doActivate();
 		getContent().addPropertyChangeListener(objectObserver);
 
-//		getViewer()
-//			.getAdapter(FocusModel.class)
-//			.addPropertyChangeListener( this::handleFocusModelUpdate );
-//	
+		FocusModel<Node> focusModel = getRoot().getViewer().getAdapter(
+				new TypeToken<FocusModel<Node>>() {});
+
+		focusModel.focusProperty().addListener(focusObserver);
 	}
 
+	@SuppressWarnings("serial")
 	@Override
 	protected void doDeactivate() {
 		getContent().removePropertyChangeListener(objectObserver);
 
-//		getViewer()
-//			.getAdapter(FocusModel.class)
-//			.removePropertyChangeListener( this::handleFocusModelUpdate );
-		
+		FocusModel<Node> focusModel = getRoot().getViewer().getAdapter(
+				new TypeToken<FocusModel<Node>>() {});
+		focusModel.focusProperty().removeListener(focusObserver);
+
 		super.doDeactivate();
 	}
 
-	
 	@Override
 	public TextNode getContent() {
-		return (TextNode)super.getContent();
+		return (TextNode) super.getContent();
 	}
 
 	@Override
@@ -79,52 +102,49 @@ public class TextNodePart extends AbstractFXContentPart<StackPane> {
 		text = new Text();
 		fxRoundedRectNode = new GeometryNode<>();
 		editText = new TextField();
-		
+
 		editText.setManaged(false);
 		editText.setVisible(false);
-		
+
 		group.getChildren().add(fxRoundedRectNode);
 		group.getChildren().add(text);
 		group.getChildren().add(editText);
-		
-		
+
 		return group;
 	}
 
 	@Override
 	protected void doRefreshVisual(StackPane visual) {
 		TextNode model = getContent();
-		
-		Font font = Font.font("Monospace", FontWeight.BOLD, 50 );
+
+		Font font = Font.font("Monospace", FontWeight.BOLD, 50);
 		Color textColor = Color.BLACK;
 		int textStrokeWidth = 2;
-		
-		
-		text.setText( model.getText() );
-		text.setFont( font );
+
+		text.setText(model.getText());
+		text.setFont(font);
 		text.setFill(textColor);
 		text.setStrokeWidth(textStrokeWidth);
 
 		// measure size
-		Bounds textBounds = msrText(model.getText(), font, textStrokeWidth );
+		Bounds textBounds = msrText(model.getText(), font, textStrokeWidth);
 
-		Rectangle bounds = new Rectangle( 
-				0, 0, 
-				textBounds.getWidth() + textBounds.getHeight(), textBounds.getHeight() * 1.5 );
+		Rectangle bounds = new Rectangle(0, 0, textBounds.getWidth() + textBounds.getHeight(),
+				textBounds.getHeight() * 1.5);
 
 		// the rounded rectangle
-		RoundedRectangle roundRect = new RoundedRectangle( bounds, 10, 10 );
+		RoundedRectangle roundRect = new RoundedRectangle(bounds, 10, 10);
 		fxRoundedRectNode.setGeometry(roundRect);
-		fxRoundedRectNode.setFill( model.getColor() );
-		fxRoundedRectNode.setStroke( Color.BLACK );
+		fxRoundedRectNode.setFill(model.getColor());
+		fxRoundedRectNode.setStroke(Color.BLACK);
 		fxRoundedRectNode.setStrokeWidth(2);
 		fxRoundedRectNode.toBack();
 
 		text.toFront();
-		
+
 		editText.toFront();
 		editText.setPrefWidth(bounds.getWidth());
-			
+
 		{
 			Point position = model.getPosition();
 			Affine affine = getAdapter(FXTransformPolicy.TRANSFORM_PROVIDER_KEY).get();
@@ -132,18 +152,10 @@ public class TextNodePart extends AbstractFXContentPart<StackPane> {
 			affine.setTy(position.y);
 		}
 	}
-	
-	private void handleFocusModelUpdate(PropertyChangeEvent evt) {
-		// when focus goes away, cancel editing
-		if( evt.getNewValue() != this  ){
-			editModeEnd(false);
-		}
-	}
-	
 
 	private Bounds msrText(String string, Font font, int textStrokeWidth) {
 		Text msrText = new Text(string);
-		msrText.setFont( font );
+		msrText.setFont(font);
 		msrText.setStrokeWidth(textStrokeWidth);
 
 		new Scene(new Group(msrText));
@@ -152,28 +164,27 @@ public class TextNodePart extends AbstractFXContentPart<StackPane> {
 	}
 
 	public void setPosition(Point newPos) {
-		getContent().setPosition( newPos );
+		getContent().setPosition(newPos);
 	}
-	
+
 	public void editModeStart() {
 		if (isEditing) {
 			return;
 		}
-			
-		
+
 		isEditing = true;
 		setVisualsForEditing();
-		
+
 		editText.setText(text.getText());
 		editText.requestFocus();
 		refreshVisual();
 	}
-	
-	public void editModeEnd( boolean commit ) {
+
+	public void editModeEnd(boolean commit) {
 		if (!isEditing) {
 			return;
 		}
-		if( commit ){
+		if (commit) {
 			String newText = editText.getText();
 			text.setText(newText);
 			getContent().setText(newText);
@@ -182,13 +193,13 @@ public class TextNodePart extends AbstractFXContentPart<StackPane> {
 		setVisualsForEditing();
 	}
 
-	private void setVisualsForEditing(){
+	private void setVisualsForEditing() {
 		editText.setManaged(isEditing);
 		editText.setVisible(isEditing);
 		text.setManaged(!isEditing);
 		text.setVisible(!isEditing);
-		
 	}
+
 	public boolean isEditing() {
 		return isEditing;
 	}
